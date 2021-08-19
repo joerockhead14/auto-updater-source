2b5c34db3db2918df7994c830f93d1b39cff495d
Joe Rockhead <84399953+joerockhead14@users.noreply.github.com>
2021-08-19 12:12:46 +0300

    Add files via upload

---
+++ auto updater s/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace auto_updater_s
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.siticoneButton1 = new Siticone.UI.WinForms.SiticoneButton();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel2 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.SuspendLayout();
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.BorderRadius = 2;
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoveredState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(46, 189);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(180, 45);
+            this.siticoneButton1.TabIndex = 0;
+            this.siticoneButton1.Text = "Kontrol Et";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.siticoneLabel1.Location = new System.Drawing.Point(12, 12);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(96, 22);
+            this.siticoneLabel1.TabIndex = 1;
+            this.siticoneLabel1.Text = "Güncelleme : ";
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.siticoneLabel2.Location = new System.Drawing.Point(110, 13);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(31, 22);
+            this.siticoneLabel2.TabIndex = 2;
+            this.siticoneLabel2.Text = "Yok";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 246);
+            this.Controls.Add(this.siticoneLabel2);
+            this.Controls.Add(this.siticoneLabel1);
+            this.Controls.Add(this.siticoneButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel2;
+    }
+}
+
---
+++ auto updater s/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+using System.Diagnostics;
+
+namespace auto_updater_s
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int version = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            string site = "";//version çekileceği url yazın.
+            WebRequest ew = HttpWebRequest.Create(site);
+            WebResponse y;
+            y = ew.GetResponse();
+            StreamReader inf = new StreamReader(y.GetResponseStream());
+            string cm = inf.ReadToEnd();
+            int st = cm.IndexOf("<p>") + 3;
+            int bitis = cm.Substring(st).IndexOf("</p>");
+            string joe = cm.Substring(st, bitis);
+            version = Convert.ToInt16(joe);
+
+
+            if (version == 1)
+            {
+                DialogResult secenek = MessageBox.Show("Güncelleme mevcut değil programı kullanmaya devam edebilirsiniz", "Joe Rockhead", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (secenek == DialogResult.Yes)
+                {
+                    siticoneLabel2.Text = "Yok";
+
+
+
+                }
+                else if (secenek == DialogResult.No)
+                {
+                    siticoneLabel2.Text = "Yok";
+
+                }
+            }
+            else
+            {
+                DialogResult secenek = MessageBox.Show("Güncelleme mevcut şimdi güncellemek istermisiniz?", "Joe Rockhead", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (secenek == DialogResult.Yes)
+                {
+                    Process.Start("");//güncelleme olduğu zaman messageboxda evete basılınca yönlendirilecek url gir
+                    siticoneLabel2.Text = "Var";
+
+
+
+                }
+                else if (secenek == DialogResult.No)
+                {
+                    siticoneLabel2.Text = "Var";
+                    
+
+                }
+
+
+            }
+        }
+    }
+}
